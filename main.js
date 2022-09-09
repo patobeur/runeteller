27249bb59d2d6bab8453b68408d4a333c11430b5
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2022-09-09 22:53:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let RuneTellerDatas, JsonTextarea, Map, Update
+let Menu, MenuMaps, MenuPoi, MenuPoiItems, MenuType, MenuIco
+let icosize = 7 // half css ico size in px
+let currentMap = 0
+let MapImagePath = "./runeteller_map/"
+window.onload = () => {
+	init()
+}
+
+
