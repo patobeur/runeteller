ba01a514e2112a3df6e0c34caefefb203f3e0938
Patobeur <patobeur41@gmail.com>
2022-09-11 12:02:06 +0200

    maj

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+let spoil = true
+let RuneTellerDatas, JsonTextarea, Map, Update
+let Menu, MenuMaps, MenuPoi, MenuPoiItems, MenuType, MenuIco
+let icosize = 7 // half css ico size in px
+let currentMap = 0
+let MapImagePath = "./assets/maps/"
+window.onload = () => {
+	init()
+}
+
+
